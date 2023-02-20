b468a22eb29232ec2691ec19634849ebd88735e8
Minji287 <dpa357@gmail.com>
2023-02-20 17:42:33 +0900

    Day 22

---
+++ Beakjoon/src/input_output/B_11022.java
@@ -0,0 +1,35 @@
+package input_output;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_11022 {
+
+	public static void main(String[] args) throws IOException {
+		/*
+		 * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+		 * 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+		 * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+		 * 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. 
+		 * x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+		 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= t; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			sb.append("Case #" + i + ": " + A + " + " + B + " = " + (A+B) + "\n");
+		}
+		br.close();
+		System.out.println(sb);
+	}
+
+}
