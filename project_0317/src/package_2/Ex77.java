4a0ccd8028eb1df7aa351f5cf80b91138e41a105
Minji287 <dpa357@gmail.com>
2023-03-17 17:33:42 +0900

    Day 47

--- project_0317/src/package_2/Ex77.java
+++ project_0317/src/package_2/Ex77.java
@@ -13,15 +13,11 @@ public class Ex77 {
 //	Ex77() {}
 	public static void main(String[] args) {
 		// Object (a, b) -> a > b ? a : b; 람다식, 익명객체
-		MyFunction f = new MyFunction() {
-			public int max(int a, int b) {
-				return a > b ? a : b;
-			}
-		};
+		MyFunction f = (a, b) -> a > b ? a : b;
 		
 		int value = f.max(3, 5);
 		System.out.println("value = " + value);
-	}
+		}
 
 //	public int max(int a, int b) {
 //		return a > b ? a : b;
